ee597c809fbf0cd0715145209a0ec2d55d240ea2
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2021-03-11 18:59:36 +0100

    Add test file

---
+++ scio-macros/src/main/scala-3/com/spotify/scio/TestSysPropsMacros.scala
@@ -0,0 +1,8 @@
+package com.spotify.scio
+
+object Maxime extends SysProps {
+  val TmpDir: SysProp = SysProp("java.io.tmpdir", "java temporary directory")
+
+    def properties: List[SysProp] = SysPropsMacros.propertiesIn(this)
+
+}
